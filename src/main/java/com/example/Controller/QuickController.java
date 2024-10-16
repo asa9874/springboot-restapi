95694ed2d0ebf8b54e1a3a785271050227ff91ef
asa9874 <pjkpjk1129@gmail.com>
2024-10-16 13:38:27 +0900

    Feat:쿼리파라미터, 경로변수

--- src/main/java/com/example/Controller/QuickController.java
+++ src/main/java/com/example/Controller/QuickController.java
@@ -1,6 +1,8 @@
 package com.example.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,15 @@ public class QuickController {
     @GetMapping("/dummy")
     public String dummy(){
         return "{}";
+    }
+
+    @GetMapping("/member")
+    public String getMember(@RequestParam("empNo") String empNo){
+        return "ok";
+    }
+
+    @GetMapping("/member2/{empNo}")
+    public String getMember2(@PathVariable("empNo") String empNo){
+        return "ok";
     }
 }
